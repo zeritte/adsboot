6f1736f763a9e2bec676fa84d7cf59de8b7539cf
Burak Dağlı <burak0206@gmail.com>
2019-12-12 21:49:31 +0300

    datatabledata removed

--- src/pages/tables/Tables.js
+++ src/pages/tables/Tables.js
@@ -12,18 +12,6 @@ import axios from 'axios'
 // data
 import mock from "../dashboard/mock";
 
-const datatableData = [
-  ["1386047344928","ENABLED" ,"The new Solaris onlineshop", "25 years of style",	"More than 50 exclusive, popular and luxury sunglasses brands.","https://www.solarisco.com/TR_EN/", "Generic","Brand_Solaris_TR_EN",  "Solaris1"],
-["386047344931","ENABLED" ,"Solaris sunglasses", "The new onlineshop","More than 50 exclusive sunglasses brands for him & her.", "https://www.solarisco.com/TR_EN/	82524424681","Generic","Brand_Solaris&Other Brands_TR_TR" , "Solaris2"],
-["386047344934","ENABLED" ,"Solaris sunglasses",	"10% off your first order",	"Exclusive sunglass brands & models. Market’s most diverse assortment." ,"https://www.solarisco.com/TR_EN/"	,"Woman","Brands_Burberry_TR_EN	1", "Solaris3"],
-["386047404469","ENABLED" ,"Boss Orange Güne? Gözlü?ü","?lk Al??veri?te %10 ?ndirim",	"Boss Orange güne? gözlüklerinde Solaris avantajlar?n? kaç?rmay?n.","https://www.solarisco.com/TR_TR/boss-orange","Woman","Brands_Emporio Armani_TR_EN", "Solaris3"],
-["386047344928","ENABLED" ,"The new Solaris onlineshop", "25 years of style",	"More than 50 exclusive, popular and luxury sunglasses brands.","https://www.solarisco.com/TR_EN/", "Generic","Brand_Solaris_TR_EN",  "Solaris1"],
-["386047344931","ENABLED" ,"Solaris sunglasses", "The new onlineshop","More than 50 exclusive sunglasses brands for him & her.", "https://www.solarisco.com/TR_EN/	82524424681","Generic","Brand_Solaris&Other Brands_TR_TR" , "Solaris2"],
-["386047344934","ENABLED" ,"Solaris sunglasses",	"10% off your first order",	"Exclusive sunglass brands & models. Market’s most diverse assortment." ,"https://www.solarisco.com/TR_EN/"	,"Woman","Brands_Burberry_TR_EN	1", "Solaris3"],
-["386047404469","ENABLED" ,"Boss Orange Güne? Gözlü?ü","?lk Al??veri?te %10 ?ndirim",	"Boss Orange güne? gözlüklerinde Solaris avantajlar?n? kaç?rmay?n.","https://www.solarisco.com/TR_TR/boss-orange","Woman","Brands_Emporio Armani_TR_EN", "Solaris3"],
-];
-
-
 export default function Tables() {
 
   const [adsDataTable,setAdsDataTable] = useState([])
