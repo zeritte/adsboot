e9f2f39619fc70bd6773b0033a1ad5c87c821cb5
zeritte <zeritte@gmail.com>
2019-12-12 22:36:06 +0300

    store & connection to app

---
+++ src/store.js
@@ -0,0 +1,38 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { createWhitelistFilter } from "redux-persist-transform-filter";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+
+export const history = History.createBrowserHistory();
+
+const initialState = {};
+const enhancers = [];
+const middleware = [thunk];
+
+if (process.env.NODE_ENV === "development") {
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+  if (typeof devToolsExtension === "function") {
+    enhancers.push(devToolsExtension());
+  }
+}
+
+const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
+
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+  transforms: [createWhitelistFilter("main", ["isLoggedIn"])],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = createStore(persistedReducer, initialState, composedEnhancers);
+
+const persistor = persistStore(store);
+
+export { store, persistor };
