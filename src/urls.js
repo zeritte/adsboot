7e4e6424b39c5b2e897bfa0a059ee26b278ebcdd
Burak Dağlı <burak0206@gmail.com>
2019-12-15 02:11:41 +0300

    signup url changed

--- src/urls.js
+++ src/urls.js
@@ -1,7 +1,7 @@
 const base_url = "https://adsbot-api.herokuapp.com/dashboard/"; // production
 
 const session = `${base_url}login`;
-const registration = `${base_url}createUser`;
+const registration = `${base_url}signup`;
 const password = `${base_url}passwords`;
 const getAllAds = `${base_url}getAds`;
 const runFilter = `${base_url}run`;
