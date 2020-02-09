b4883003f637256a1d992a95d6b0d7c7ce5c3b78
zeritte <zeritte@gmail.com>
2020-02-09 23:21:19 +0300

    removed warning

--- src/helpers/datatable/MUIDataTable.js
+++ src/helpers/datatable/MUIDataTable.js
@@ -412,11 +412,6 @@ class MUIDataTable extends React.Component {
         'The "scroll" responsive option has been deprecated. It is being replaced by "scrollMaxHeight"',
       );
     }
-    if (this.options.fixedHeader) {
-      console.error(
-        "fixedHeader has been deprecated in favor of fixedHeaderOptions: { xAxis: boolean, yAxis: boolean }. Once removed, the new options will be set by default to render like the old fixedHeader. However, if you are setting the fixedHeader value manually, it will no longer work in the next major version.",
-      );
-    }
 
     this.props.columns.map(c => {
       if (c.options && c.options.customFilterListRender) {
