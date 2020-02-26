e3455d534d2170100326c39ca78bd12fb264749a
zeritte <zeritte@gmail.com>
2020-02-27 01:12:25 +0300

    disable header mock

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -39,56 +39,56 @@ import { connect } from "react-redux";
 import { logoutUser, selectProject } from "../../actions";
 
 const messages = [
-  {
-    id: 0,
-    variant: "warning",
-    name: "Jane Hew",
-    message: "Hey! How is it going?",
-    time: "9:32",
-  },
-  {
-    id: 1,
-    variant: "success",
-    name: "Lloyd Brown",
-    message: "Check out my new Dashboard",
-    time: "9:18",
-  },
-  {
-    id: 2,
-    variant: "primary",
-    name: "Mark Winstein",
-    message: "I want rearrange the appointment",
-    time: "9:15",
-  },
-  {
-    id: 3,
-    variant: "secondary",
-    name: "Liana Dutti",
-    message: "Good news from sale department",
-    time: "9:09",
-  },
+  // {
+  //   id: 0,
+  //   variant: "warning",
+  //   name: "Jane Hew",
+  //   message: "Hey! How is it going?",
+  //   time: "9:32",
+  // },
+  // {
+  //   id: 1,
+  //   variant: "success",
+  //   name: "Lloyd Brown",
+  //   message: "Check out my new Dashboard",
+  //   time: "9:18",
+  // },
+  // {
+  //   id: 2,
+  //   variant: "primary",
+  //   name: "Mark Winstein",
+  //   message: "I want rearrange the appointment",
+  //   time: "9:15",
+  // },
+  // {
+  //   id: 3,
+  //   variant: "secondary",
+  //   name: "Liana Dutti",
+  //   message: "Good news from sale department",
+  //   time: "9:09",
+  // },
 ];
 
 const notifications = [
-  { id: 0, color: "warning", message: "Check out this awesome ticket" },
-  {
-    id: 1,
-    color: "success",
-    type: "info",
-    message: "What is the best way to get ...",
-  },
-  {
-    id: 2,
-    color: "secondary",
-    type: "notification",
-    message: "This is just a simple notification",
-  },
-  {
-    id: 3,
-    color: "primary",
-    type: "e-commerce",
-    message: "12 new orders has arrived today",
-  },
+  // { id: 0, color: "warning", message: "Check out this awesome ticket" },
+  // {
+  //   id: 1,
+  //   color: "success",
+  //   type: "info",
+  //   message: "What is the best way to get ...",
+  // },
+  // {
+  //   id: 2,
+  //   color: "secondary",
+  //   type: "notification",
+  //   message: "This is just a simple notification",
+  // },
+  // {
+  //   id: 3,
+  //   color: "primary",
+  //   type: "e-commerce",
+  //   message: "12 new orders has arrived today",
+  // },
 ];
 
 function Header(props) {
@@ -205,7 +205,7 @@ function Header(props) {
         >
           <AccountIcon classes={{ root: classes.headerIcon }} />
         </IconButton>
-        <Menu
+        {/* <Menu
           id="mail-menu"
           open={Boolean(mailMenu)}
           anchorEl={mailMenu}
@@ -259,8 +259,8 @@ function Header(props) {
             Send New Message
             <SendIcon className={classes.sendButtonIcon} />
           </Fab>
-        </Menu>
-        <Menu
+        </Menu> */}
+        {/* <Menu
           id="notifications-menu"
           open={Boolean(notificationsMenu)}
           anchorEl={notificationsMenu}
@@ -277,7 +277,7 @@ function Header(props) {
               <Notification {...notification} typographyVariant="inherit" />
             </MenuItem>
           ))}
-        </Menu>
+        </Menu> */}
         <Menu
           id="profile-menu"
           open={Boolean(profileMenu)}
