84e44eda2c09d29950356523e52b29fed71487ab
zeritte <zeritte@gmail.com>
2020-07-23 21:34:23 +0300

    update url back again

--- src/urls.js
+++ src/urls.js
@@ -1,5 +1,4 @@
-const base_url =
-  "http://adsbotprod-env.eba-jtuvxfnb.eu-west-3.elasticbeanstalk.com/"; // production
+const base_url = "https://adsbotapi.herokuapp.com/api/"; // production
 
 const session = `${base_url}token`;
 const registration = `${base_url}user`;
